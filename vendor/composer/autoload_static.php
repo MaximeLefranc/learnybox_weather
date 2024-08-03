03f79fc65530ae1d0b2957e91dfdc4b714c1a34f
Maxime <maxilefranc@gmail.com>
2024-08-03 12:54:24 +0200

    symfony vardumper removed

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,17 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit938dc3bf5cd7117cb128f363527707a1
 {
     public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         '94d1ccdaf3f92627513d5c77d88f8e94' => __DIR__ . '/../..' . '/config/config.php',
     );
 
     public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'Symfony\\Polyfill\\Mbstring\\' => 26,
-            'Symfony\\Component\\VarDumper\\' => 28,
-        ),
         'P' => 
         array (
             'PhpTest\\' => 8,
@@ -25,14 +18,6 @@ class ComposerStaticInit938dc3bf5cd7117cb128f363527707a1
     );
 
     public static $prefixDirsPsr4 = array (
-        'Symfony\\Polyfill\\Mbstring\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
-        ),
-        'Symfony\\Component\\VarDumper\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/var-dumper',
-        ),
         'PhpTest\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -37,23 +37,5 @@
             'aliases' => array(),
             'dev_requirement' => false,
         ),
-        'symfony/polyfill-mbstring' => array(
-            'pretty_version' => 'v1.30.0',
-            'version' => '1.30.0.0',
-            'reference' => 'fd22ab50000ef01661e2a31d850ebaa297f8e03c',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../symfony/polyfill-mbstring',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'symfony/var-dumper' => array(
-            'pretty_version' => 'v7.1.3',
-            'version' => '7.1.3.0',
-            'reference' => '86af4617cca75a6e28598f49ae0690f3b9d4591f',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../symfony/var-dumper',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
     ),
 );
